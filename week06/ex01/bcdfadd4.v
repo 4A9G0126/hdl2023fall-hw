408f50328aab0c81eaea1d741700e46a0f176837
4A9G0126 <133637395+4A9G0126@users.noreply.github.com>
2023-10-25 06:05:18 +0000

    codespace

--- week06/ex01/bcdfadd4.v
+++ week06/ex01/bcdfadd4.v
@@ -5,6 +5,18 @@ module bcdfadd4(
   output wire [3:0] sum,
   output wire cout
 );
+  reg [4:0]_sum;
+
+  assign sum =_sum[3:0];
+  assign cout = _sum[4];
+
+  always @* begin
+  _sum = a + b + cin;
+  if (_sum > 9) begin
+  _sum = _sum-10;
+  _sum[4] = 1'b1;
+  end
+end
 
   // write your code here
   
